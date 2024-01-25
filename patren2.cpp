bbae4dd932955a84df82f8cbc7dfaa748aa863d7
Amansharma529201 <amankumar529201@gmail.com>
2024-01-26 04:11:45 +0530

    pattern questions

--- patren2.cpp
+++ patren2.cpp
@@ -57,3 +57,22 @@ int main(){
         cout<<endl;
     }
 }*/
+
+//<----------------------------------------------------------------------------->
+//number tree
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cin>>n;
+    for(int i= 1;i <=n;i++){
+        for(int j =1; j<= n-i;j++){
+            cout<<" ";
+        }
+        for(int j= 1; j <= i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
