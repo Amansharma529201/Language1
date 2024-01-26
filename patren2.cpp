0ff7643db8a3afd57d79cc4cfb44ce8b270cdf3d
Amansharma529201 <amankumar529201@gmail.com>
2024-01-26 18:30:34 +0530

    updated

--- patren2.cpp
+++ patren2.cpp
@@ -60,7 +60,7 @@ int main(){
 
 //<----------------------------------------------------------------------------->
 //number tree
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 
 int main (){
@@ -72,6 +72,32 @@ int main (){
         }
         for(int j= 1; j <= i;j++){
             cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}*/
+
+//<---------------------------------------------------------------------------------------->
+//Creating a palindrom pattern 
+
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cin>>n;
+    for(int i =1;i<=n;i++){
+        int j;
+        for(j = 1;j<=n-i;j++){
+            cout<<" ";
+        }
+        int k=i;
+        for(;j<=n;j++){
+            cout<<k--;
+        }
+        k=2;
+        for(;j<=n+i-1;j++){
+            cout<<k++;
         }
         cout<<endl;
     }
